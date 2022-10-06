66ca6e9d38ce40b9b5182835b51a0201be0fd878
obook <obooklage@free.fr>
2022-10-06 16:30:49 +0200

    Set the font size

--- TeX2Board.wgt/scripts/index.js
+++ TeX2Board.wgt/scripts/index.js
@@ -11,7 +11,10 @@ function ContentLoaded()
 {
     console.log("ContentLoaded.");
     window.hiddenbuttons = false;
+    window.defaultfontsize = 100; /* 100% */
+    window.fontsize = window.defaultfontsize; /* 100% */
     GetParameters();
+    // FontSizeDefault();
     /* Futurs use
     if (window.widget) 
     {  
@@ -30,6 +33,8 @@ function ContentLoaded()
 
 function GetParameters()
 {
+    
+    console.log("GetParameters");
     /* Restauration du code source */
     
     if (window.sankore)
@@ -40,30 +45,38 @@ function GetParameters()
             if( MathInput != "")
             {
                 document.getElementById("MathInput").value = MathInput;
-                console.log("Préférences sankore récupérées.");
+            }
+            else
+            {
+                MakeSampleCode();
             }
             window.hiddenbuttons = (window.sankore.preference('hiddenbuttons', false) === 'true');
+            window.fontsize = parseInt(window.sankore.preference('fontsize', 100));
+            console.log("Préférences sankore récupérées.");
         }
-        else
-        {
-            MakeSampleCode();
-        }
+
     }
-    else if (window.localStorage.getItem('MathInput'))
-    {
-        var MathInput = window.localStorage.getItem('MathInput');
-        if( MathInput !="" )
+    else 
+    {    
+        if (window.localStorage.getItem('MathInput'))
         {
-            document.getElementById("MathInput").value=window.localStorage.getItem('MathInput');
-            console.log("Préférences localStorage récupérées.");
+            var MathInput = window.localStorage.getItem('MathInput');
+            if( MathInput !="" )
+            {
+                document.getElementById("MathInput").value=window.localStorage.getItem('MathInput');
+                console.log("Préférences localStorage récupérées.");
+            }
+            else /* Sample TeX */
+            {
+                MakeSampleCode();
+            }
         }
         window.hiddenbuttons = (window.localStorage.getItem('hiddenbuttons', false) === 'true');
-    }
-    else /* Sample TeX */
-    {
-        MakeSampleCode();
+        window.fontsize = parseInt(window.localStorage.getItem('fontsize', 100));
     }
 
+    FontSizeSet(window.fontsize);
+    
     SetButtonsState(window.hiddenbuttons);
 
     MakeLaTeX();
@@ -86,6 +99,7 @@ function SaveParameters()
             window.sankore.setPreference('parameters','saved');
             window.sankore.setPreference('MathInput', MathInput);
             window.sankore.setPreference('hiddenbuttons', window.hiddenbuttons);
+            window.sankore.setPreference('fontsize', window.fontsize);
             console.log("Préférences sankore sauvegardées.");
         }
     }
@@ -93,6 +107,7 @@ function SaveParameters()
     {
         localStorage.setItem('MathInput', MathInput);
         localStorage.setItem('hiddenbuttons', window.hiddenbuttons);
+        localStorage.setItem('fontsize', window.fontsize);
         console.log("Préférences localstorage sauvegardées.");
     }
 }
@@ -122,21 +137,59 @@ function SwitchButtonsState()
     const HideShowButton = document.getElementById("HideShowButton");
     const MakeLaTeXButton = document.getElementById("MakeLaTeXButton");
     const MathInput = document.getElementById("MathInput");
-
+    const FontSizeDefault = document.getElementById("FontSizeDefault");
+    const FontSizeDown = document.getElementById("FontSizeDown");
+    const FontSizeUp = document.getElementById("FontSizeUp");
+    
     if(window.hiddenbuttons === true)
     {
         MathInput.style.display = "block";
+        HideShowButton.textContent = "HIDE";
+                
         MakeLaTeXButton.style.visibility = 'visible';
-        HideShowButton.value = "Hide";
+        FontSizeDefault.style.visibility = 'visible';
+        FontSizeDown.style.visibility = 'visible';
+        FontSizeUp.style.visibility = 'visible';
+        MakeLaTeXButton.style.visibility = 'visible';
         window.hiddenbuttons = false;
     }
     else
     {
         MathInput.style.display = "none";
+        HideShowButton.textContent = "SHOW";
+        
         MakeLaTeXButton.style.visibility = 'hidden';
-        HideShowButton.value = "Show";
+        FontSizeDefault.style.visibility = 'hidden';
+        FontSizeDown.style.visibility = 'hidden';
+        MakeLaTeXButton.style.visibility = 'hidden';
+        FontSizeUp.style.visibility = 'hidden';
         window.hiddenbuttons = true;
     }
 
     SaveParameters();
+}
+
+function FontSizeSet(value)
+{
+    console.log("FontSizeSet="+value);
+    document.getElementsByTagName('body')[0].style.fontSize = value +"%";
+    SaveParameters();
+}
+
+function FontSizeDefault()
+{
+    window.fontsize = window.defaultfontsize;
+    FontSizeSet(window.fontsize);
+}
+
+function FontSizeUp()
+{
+    window.fontsize = window.fontsize +20;
+    FontSizeSet(window.fontsize);
+}
+
+function FontSizeDown()
+{
+    window.fontsize = window.fontsize -20;
+    FontSizeSet(window.fontsize);
 }
