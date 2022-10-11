eba1a985eff3d6c0ee1101832e13d3ed08dedd05
obook <obooklage@free.fr>
2022-10-11 09:38:58 +0200

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,2 @@
+REM rd /s /q "%UserProfile%\AppData\Local\OpenBoard\interactive content\TeX2Board.wgt"
+xcopy /e /k /h /i TeX2Board.wgt "%UserProfile%\AppData\Local\OpenBoard\interactive content\TeX2Board.wgt"
