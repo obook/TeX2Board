818b347fd63f27424993484ddf5600236b59f4c6
obook <obooklage@free.fr>
2022-10-17 21:49:54 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,2 +1,3 @@
 REM rd /s /q "%UserProfile%\AppData\Local\OpenBoard\interactive content\TeX2Board.wgt"
 xcopy /e /k /h /i TeX2Board.wgt "%UserProfile%\AppData\Local\OpenBoard\interactive content\TeX2Board.wgt"
+cmd /c pause
