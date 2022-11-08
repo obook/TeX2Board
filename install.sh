091940f18a2c032dcc01ee60dfa3a7d90116a728
obook <obooklage@free.fr>
2022-11-08 21:14:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rm -r ~/.local/share/OpenBoard/interactive\ content/TeX2Board.wgt
+# rm -r ~/.local/share/OpenBoard/interactive\ content/TeX2Board.wgt
 cp -avR TeX2Board.wgt ~/.local/share/OpenBoard/interactive\ content/
